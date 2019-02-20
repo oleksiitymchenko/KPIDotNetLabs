acd4bbb612bb791a923fae03ee1938cedfa6a629
Oleksii Tymchenko <olexiytimchenko@gmail.com>
2019-02-20 17:03:24 +0200

    copy

---
+++ AcademicPerformanceUI/AcademicPerformanceUI/Views/MainWindow - Copy.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using AcademicPerformanceUI.ViewModels;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using MenuItem = AcademicPerformanceUI.ViewModels.MenuItem;
+
+
+namespace AcademicPerformanceUI.Views
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Navigation.Navigation.Frame = new Frame() { NavigationUIVisibility = NavigationUIVisibility.Hidden };
+            Navigation.Navigation.Frame.Navigated += SplitViewFrame_OnNavigated;
+
+            //this.Loaded += (sender, args) => Navigation.Navigation.Navigate(new Uri("Views/ScheduleView.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void SplitViewFrame_OnNavigated(object sender, NavigationEventArgs e)
+        {
+            this.HamburgerMenuControl.Content = e.Content;
+            this.HamburgerMenuControl.SelectedItem = e.ExtraData ?? ((MenuViewModel)this.DataContext).GetItem(e.Uri);
+            this.HamburgerMenuControl.SelectedOptionsItem = e.ExtraData ?? ((MenuViewModel)this.DataContext).GetOptionsItem(e.Uri);
+            GoBackButton.Visibility = Navigation.Navigation.Frame.CanGoBack ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void GoBack_OnClick(object sender, RoutedEventArgs e)
+        {
+            Navigation.Navigation.GoBack();
+        }
+
+        private void HamburgerMenuControl_OnItemInvoked(object sender, HamburgerMenuItemInvokedEventArgs e)
+        {
+            var menuItem = e.InvokedItem as MenuItem;
+            if (menuItem != null && menuItem.IsNavigation)
+            {
+                Navigation.Navigation.Navigate(menuItem.NavigationDestination, menuItem);
+            }
+        }
+    }
+}
