85c1fa796a2871704c907c14bc1fb0bfa7122897
Oleksii Tymchenko <olexiytimchenko@gmail.com>
2019-04-25 18:03:00 +0300

    created new project

---
+++ AcademicPerformanceUI/WcfMsmqService/DTOModels/IBaseDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WcfMsMqService.DTOModels
+{
+    public interface IBaseDto
+    {
+        Guid Id { get; set; }
+    }
+}
---
+++ AcademicPerformanceUI/WcfMsmqService/DTOModels/TestResultDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace WcfMsMqService.DTOModels
+{
+    [DataContract]
+    public class TestResultDto : IBaseDto
+    {
+        [DataMember]
+        public Guid Id { get; set; }
+
+        [DataMember]
+        public int Mark { get; set; }
+
+        [DataMember]
+        public Guid TestId { get; set; }
+
+        [DataMember]
+        public Guid StudentId { get; set; }
+    }
+}
